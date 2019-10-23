d5ac3b8c41339c343f4ede98b00f45aaa70e81a9
Chahi Rabie Ala-Eddine <rabie_chahi@yahoo.fr>
2019-10-24 00:45:08 +0200

    Update

--- src/main/java/rpn/calculator/project/src/CalculatorRPN.java
+++ src/main/java/rpn/calculator/project/src/CalculatorRPN.java
@@ -6,10 +6,29 @@ public enum CalculatorRPN
 {
 	CALCULATOR;
 	
+	public void start() 
+		throws CaptureInException, StackSizeException, AbsoluteMinMaxValuesException, DivideByZeroException
+	{
+		try 
+		{
+			CaptureRPN cptrpn = new CaptureRPN();
+			cptrpn.capture();
+		}
+		
+		catch(CaptureInException exp) {}
+		
+		catch(StackSizeException exp) {}
+		
+		catch(AbsoluteMinMaxValuesException exp) {}
+		
+		catch(DivideByZeroException exp) {}
+		
+	}
+	
 	public static void main(String[] args) 
 		throws AbsoluteMinMaxValuesException, DivideByZeroException, StackSizeException, CaptureInException 
 	{
-		CaptureRPN rpn = new CaptureRPN();
-		rpn.capture();
+		CALCULATOR.start();
 	}
+
 }
